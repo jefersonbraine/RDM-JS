4290dd8db97ab4b43aa8fd7f0e8e59a4d7149ccc
Jeferson <jefeleal363@gmail.com>
2024-08-06 14:51:51 -0300

    feat: G.B Desafios 01 e 02

---
+++ cursoemvideo/DESAFIOS/D001/script.js
@@ -0,0 +1,19 @@
+
+function alerta1() {
+
+    window.alert('Voce clicou no botão 1')
+    
+}
+function alerta2() {
+
+    window.alert('Voce clicou no botão 2')
+    
+}
+
+function alerta3() {
+
+    window.alert('Voce clicou no botão 3')
+    
+}
+
+
---
+++ cursoemvideo/DESAFIOS/D002/script.js
@@ -0,0 +1,9 @@
+
+function iniciate() {
+
+    let nome = window.prompt('Qual seu nome?')
+    let idade = window.prompt('Qual a sua idade?')
+
+    window.alert(`Acabei de conhecer! ${nome}, que tem ${idade} anos de idade.`)
+      
+}
