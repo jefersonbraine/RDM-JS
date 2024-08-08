728933679850280cfad27be9451503f4bb942b80
Jeferson <jefeleal363@gmail.com>
2024-08-08 15:09:26 -0300

    feat: fund. DOM

--- cursoemvideo/AULAS/aula09/script.js
+++ cursoemvideo/AULAS/aula09/script.js
@@ -4,7 +4,21 @@ var p1 = window.document.getElementsByTagName('p')[1]
 window.document.write(`Esta escrito assim: ${p1.innerText} <br>`) // vem sem o negrito, não pega a formatação
 
 
-document.write(p1.innerHTML)// o innerHTML aparece com o negrito, pega a formatação, aparece até nos alerts
+//document.write(p1.innerHTML)// o innerHTML aparece com o negrito, pega a formatação, aparece até nos alerts
 //corpo.style.background = 'green'
 //p1.style.color = 'blue' muda a cor do texto do primeiro parágrado para azul
 
+/*
+var d = document.getElementById('msg')
+d.style.background = 'green'
+d.innerHTML = 'Estou aguardando...'
+// window.document.getElementById('msg').innerText = 'Olá'
+*/
+
+/*
+var d = document.getElementsByName('msg')[0]
+d.innerText = 'Estou aguardando... com name'
+*/
+
+//var d = document.querySelector('div#msg')//id
+//var d = document.querySelector('div.msg')//class
