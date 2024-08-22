105d5adbdc11cbfe72fa77b8d26dcc02ce5bd650
Jeferson <jefeleal363@gmail.com>
2024-08-21 23:09:50 -0300

    feat: organized

--- cursoemvideo/AULAS/aula11/ex008.js
+++
@@ -1,9 +0,0 @@
-var vel = 58.5;
-
-console.log(`A velocidade do seu carro e ${vel} km/h`);
-
-if (vel > 60) { // condicão simples
-  console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`);
-}
-
-console.log(`Dirija sempre usando cinto de segurança!`);
--- cursoemvideo/DESAFIOS/D002/script.js
+++
@@ -1,9 +0,0 @@
-
-function iniciate() {
-
-    let nome = window.prompt('Qual seu nome?')
-    let idade = window.prompt('Qual a sua idade?')
-
-    window.alert(`Acabei de conhecer! ${nome}, que tem ${idade} anos de idade.`)
-      
-}
